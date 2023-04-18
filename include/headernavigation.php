6faaaed7d901ca6c0de1ef41a6bde10d6f35b9e4
Sarvesh Raje <sarvesh.infinite@gmail.com>
2023-04-18 16:41:31 +0530

    Database Added

---
+++ include/headernavigation.php
@@ -0,0 +1,18 @@
+<section class="navigation">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Dashboard</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                    aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="nav justify-content-end w-100">
+                        <li>
+                        <a href="logout.php" class="btn btn-secondary">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>        
+    </section>
