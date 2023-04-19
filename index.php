b0641bb400fd2f9d0111f504fd042b5af6789c1d
Sarvesh Raje <sarvesh.infinite@gmail.com>
2023-04-19 10:00:11 +0530

    Second Day

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title>Login File</title>
+    <title>Login</title>
 </head>
 
 <body class="bg-light">
